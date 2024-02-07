95b46dc95ec3b58f03bbef444cec8ed2badb01e9
bianyuanop <chen.me.nan@gmail.com>
2024-02-07 14:30:27 -0500

    update otel

--- trace/noop.go
+++ trace/noop.go
@@ -4,9 +4,7 @@
 package trace
 
 import (
-	"context"
-
-	oteltrace "go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/noop"
 
 	"github.com/ava-labs/avalanchego/trace"
 )
@@ -15,15 +13,7 @@ var _ trace.Tracer = (*noOpTracer)(nil)
 
 // noOpTracer is an implementation of trace.Tracer that does nothing.
 type noOpTracer struct {
-	t oteltrace.Tracer
-}
-
-func (n noOpTracer) Start(
-	ctx context.Context,
-	spanName string,
-	opts ...oteltrace.SpanStartOption,
-) (context.Context, oteltrace.Span) {
-	return n.t.Start(ctx, spanName, opts...)
+	noop.Tracer
 }
 
 func (noOpTracer) Close() error {
